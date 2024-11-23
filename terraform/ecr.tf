bdc0e33a755c0364d0b05597fe777e398b2bfb23
tanlye <tan_lovell@hotmail.com>
2024-11-23 12:01:57 +0800

    v1

---
+++ terraform/ecr.tf
@@ -0,0 +1,8 @@
+resource "aws_ecr_repository" "private_repository" {
+  name                 = var.ecr_repo
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,73 @@
+variable "vpc_id" {
+  description = "The ID of the VPC"
+  type        = string
+  default     = "vpc-0e149ba65327585a6"
+}
+
+variable "ex_role_arn" {
+    description = "The arn of execution role"
+    type        = string
+    default     = "arn:aws:iam::255945442255:role/ecsTaskExecutionRole"
+}
+
+# Security Group
+variable "sg_id" {
+    description = "The security group id"
+    type        = string
+    default     = "sg-0d27b7c2a485d0c74"
+}
+
+variable "region" {
+  description = "The AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+# Application Load Balancer
+variable "ecs_alb" {
+  type    = string
+  default = "ce7-lovell-ecs-alb"
+}
+
+
+# ECS target group
+variable "ecs_alb_tg" {
+  type    = string
+  default = "ce7-lovell-ecs-target-group"
+}
+
+#  ECR Repository
+variable "ecr_repo" {
+  type    = string
+  default = "ce7-lovell-ecr_repo"
+}
+
+# ECS Task Definition
+variable "ecs_task_def" {
+  type    = string
+  default = "ce7-lovell-wtc-task_def"
+}
+
+# ECS Port name
+variable "ecs_port_name" {
+  type    = string
+  default = "lovell-ecs-8080-tcp"
+}
+
+# ECS log group name
+variable "ecs_log_group" {
+  type    = string
+  default = "/ecs/lovelltaskdef"
+}
+
+# ECS cluster name
+variable "ecs_cluster_name" {
+  type    = string
+  default = "ce7-lovell-ecs-cluster"
+}
+
+# ECS service name
+variable "ecs_service_name" {
+  type    = string
+  default = "ce7-lovell-ecs-service"
+}
